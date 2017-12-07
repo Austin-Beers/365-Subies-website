cd43cdddfaf56cb072108a4c28ebc124f9c031db
Austin Beers <beersa6@students.rowan.edu>
2017-12-06 21:43:12 -0500

    updated css, fixed javascript and jquery

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let headerState = {gameStage: "start"}
+let headerState = {pageStage: "start"}
 function renderStartPage(){
     headerState.gameStage = "start";
   const partsButton = `<button class="parts-button">blah1</button>`;
@@ -7,24 +7,45 @@ function renderStartPage(){
   const heading = `<h1>365 Subaru</h1>`;
   $(".start-page").html(heading + partsButton + galleryButton + AAbutton);
   $(".parts-button").click(renderPartsPage);
-  $(".gallery-button").click(function(){
-      renderGalleryPage();
-    $(".start-page").html("");
+  $(".AA-button").click(renderAAPage);
+  $(".gallery-button").click(renderGalleryPage);
   //convert to form similar to parts page
 });
-  $(".AA-button").click(function(){
-      renderAAPage();
-    $(".start-page").html("");
-  //convert to form similar to parts page
-});
-  }
-  function renderPartsPage(){
-    console.log("clicked parts")
-    $(".parts-page").show()
-    $(".start-page").hide();
-    headerState.gameStage = "parts";
-    $(".parts-search-button").click(function(){
-    renderWalmartResult();
+ 
+function renderGalleryPage(){
+     $(".gallery-page").show();
+     $(".start-page").hide();
+     headerState.pageStage = "gallery";
+    }
+
+function renderAAPage(){
+  console.log("beanbag");
+  $(".aa-page").show();
+  $(".start-page").hide();
+  headerState.pageStage = "AA";
+      //   $("aa-search-button").click(function(){
+      //   const searchAA = $(".aa-search-input").val()
+      //   renderEtsyResults();
+  $(".aa-search-form").submit(event => {
+    event.preventDefault();
+    const aaTarget = $(event.currentTarget).find("aa-search-input");
+    const aaQuery = aaTarget.val();
+    partsTarget.val("");
+    getDatafromEtsy(aaQuery, displayEtsyData);
 });
 }
- renderStartPage()
+
+function renderPartsPage(){
+    console.log("clicked parts");
+    $(".parts-page").show();
+    $(".start-page").hide();
+    headerState.pageStage = "parts";
+    $(".parts-search-form").submit(event => {
+    event.preventDefault();
+    const partsTarget = $(event.currentTarget).find("parts-search-input");
+    const partsQuery = partsTarget.val();
+    partsTarget.val("");
+    getDatafromWalmart(partsQuery, displayWalmartData);
+});
+}}
+ renderStartPage();
