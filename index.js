ce45c69648602d15abafd767e6291c5a3105a78c
Austin Beers <beersa6@students.rowan.edu>
2017-12-17 22:24:24 -0500

    added mobile view css

--- index.js
+++ index.js
@@ -5,49 +5,51 @@ let headerState = {
 }
 
 function renderBackButton(){
-    const backButton = `<button class="back-button">back</button>`
-    $(".back-page").html(backButton)
-    $(".back-button").click(returnToStartPage)
-    $(".back-page").show()
+    const backButton = `<button id="back-button">back</button>`
+    $("#back-page").html(backButton)
+    $("#back-button").click(returnToStartPage)
+    $("#back-page").show()
 }
 
 function returnToStartPage(){
-    $(".start-page").show()
-    $(".back-page").hide()
-    $(".parts-page").hide()
-    $(".gallery-page").hide()
-    $(".aa-page").hide()
+    $("#start-page").show()
+    $("#back-page").hide()
+    $("#parts-page").hide()
+    $("#gallery-page").hide()
+    $("#aa-page").hide()
 }
 
 function renderStartPage() {
     headerState.gameStage = "start";
-    const partsButton = `<button class="parts-button">Search Parts</button>`;
-    const galleryButton = `<button class="gallery-button">Our Gallery</button>`;
-    const AAbutton = `<button class="AA-button">Search Accesories and Apparel </button>`;
+    const partsButton = `<button id="parts-button">Search Parts</button>`;
+    const galleryButton = `<button id="gallery-button">Our Gallery</button>`;
+    const AAbutton = `<button id="AA-button">Search Accesories and Apparel </button>`;
     const heading = `<h1>365 Subaru</h1>`;
-    $(".start-page").html(heading + partsButton + galleryButton + AAbutton);
-    $(".parts-button").click(renderPartsPage);
-    $(".AA-button").click(renderAAPage);
-    $(".gallery-button").click(renderGalleryPage);
+    $("#parts-button-id").html(partsButton);
+    $("#gallery-button-id").html(galleryButton);
+    $("#aa-button-id").html(AAbutton);
+    $("#parts-button").click(renderPartsPage);
+    $("#AA-button").click(renderAAPage);
+    $("#gallery-button").click(renderGalleryPage);
     //convert to form similar to parts page
 };
 
 function renderGalleryPage() {
     renderBackButton()
-    $(".gallery-page").show();
-    $(".start-page").hide();
+    $("#gallery-page").show();
+    $("#start-page").hide();
     headerState.pageStage = "gallery";
 }
 
 function renderAAPage() {
     renderBackButton()
     console.log("beanbag");
-    $(".aa-page").show();
-    $(".start-page").hide();
+    $("#aa-page").show();
+    $("#start-page").hide();
     headerState.pageStage = "AA";
-    $(".aa-search-form").submit(event => {
+    $("#aa-search-form").submit(event => {
         event.preventDefault();
-        const aaTarget = $(event.currentTarget).find(".aa-search-input");
+        const aaTarget = $(event.currentTarget).find("#aa-search-input");
         const aaQuery = aaTarget.val();
         console.log(aaQuery);
         aaTarget.val("");
@@ -58,13 +60,13 @@ function renderAAPage() {
 function renderPartsPage() {
     renderBackButton()
     console.log("clicked parts");
-    $(".parts-page").show();
-    $(".start-page").hide();
+    $("#parts-page").show();
+    $("#start-page").hide();
     headerState.pageStage = "parts";
-    $(".parts-search-form").submit(event => {
+    $("#parts-search-form").submit(event => {
         
         event.preventDefault();
-        const partsTarget = $(event.currentTarget).find(".parts-search-input");
+        const partsTarget = $(event.currentTarget).find("#parts-search-input");
         const partsQuery = partsTarget.val();
         partsTarget.val("");
         getDataFromWalmart(partsQuery, displayWalmartData);
@@ -92,19 +94,19 @@ function getDataFromWalmart(searchTerm, callback) {
     return `
     <div>
         <h2>
-            <a class="parts-search-title" href="${result.productUrl}" target="_blank">${result.name}</a>
-            <a class="parts-search-price" href="${result.productUrl}" target="_blank">${result.salePrice}</a>
+            <a id="parts-search-title" href="${result.productUrl}" target="_blank">${result.name}</a>
+            <a id="parts-search-price" href="${result.productUrl}" target="_blank">${result.salePrice}</a>
         </h2>
-        <a class="parts-search-image" href="${result.productUrl}" target="_blank"><img src="${result.thumbnailImage}</a>"
+        <a id="parts-search-image" href="${result.productUrl}" target="_blank"><img src="${result.thumbnailImage}</a>"
     </div>
 `
 }
 
 function displayWalmartData(data){
-    let totalResultsFnd = `<h5> Your search returned <span class='resultNum'>${data.totalResults}</span> result(s).</h5>`;
-    $(".totalNum0").html(totalResultsFnd);
+    let totalResultsFnd = `<h5> Your search returned <span id='resultNum'>${data.totalResults}</span> result(s).</h5>`;
+    $("#totalNum0").html(totalResultsFnd);
     const searchResults = data.items.map((item, index) => renderWalmartResult(item));
-    $(".parts-result").html(searchResults);
+    $("#parts-result").html(searchResults);
 }         
 
 function getDataFromEtsy(searchTerm, callback) {
@@ -125,19 +127,19 @@ function getDataFromEtsy(searchTerm, callback) {
      return `
      <div>
          <h2>
-             <a class="aa-search-title" href="${result.url}" target="_blank">${result.title}</a>
-             <a class="aa-search-price" href="${result.url}" target="_blank">${result.price}</a>
+             <a id="aa-search-title" href="${result.url}" target="_blank">${result.title}</a>
+             <a id="aa-search-price" href="${result.url}" target="_blank">${result.price}</a>
          </h2>
-         <a class="aa-search-image" href="${result.url}" target="_blank"><img src="${result.Images[0].url_75x75}"></img></a>"
+         <a id="aa-search-image" href="${result.url}" target="_blank"><img src="${result.Images[0].url_75x75}"></img></a>"
      </div>
  `
   }
   
   function displayEtsyData(data){
     console.log(data, "data")
-    let totalResultsFnd = `<h5> Your search returned <span class='resultNum'>${data.count}</span> result(s).</h5>`;
+    let totalResultsFnd = `<h5> Your search returned <span id='resultNum'>${data.count}</span> result(s).</h5>`;
     const searchResults = data.results.map((item, index) => renderEtsyResult(item));
-    $(".totalNum1").html(totalResultsFnd);
-    $(".aa-result").html(searchResults);
+    $("#totalNum1").html(totalResultsFnd);
+    $("#aa-result").html(searchResults);
 }
 renderStartPage();
